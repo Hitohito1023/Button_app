cf529aa880852ac92784b98b58ac0e9522213e58
Hitohito1023 <ryotakinase1023@gmail.com>
2021-03-25 19:25:34 +0900

    [finish]

--- app/src/main/java/com/example/buttonapp/MainActivity.java
+++ app/src/main/java/com/example/buttonapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.buttonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean buttonTap = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ボタンの設定
+        Button button = findViewById(R.id.button);
+
+        // TextViewの設定
+        textView = findViewById(R.id.text_view);
+
+        button.setOnClickListener( v -> {
+            if (buttonTap){
+                textView.setText("Hello");
+                buttonTap = false;
+            } else {
+                textView.setText("world");
+                buttonTap = true;
+            }
+        });
     }
 }
